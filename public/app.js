660fc48849c72c99cf91e49e1398cb36564743f1
Anisa <anisahamohamed@gmail.com>
2020-05-13 17:38:27 -0400

    query products where price is greaterthan

--- public/app.js
+++ public/app.js
@@ -3,17 +3,35 @@ document.addEventListener("DOMContentLoaded", (event) => {
   const app = firebase.app();
   //reference database
   const db = firebase.firestore();
+  const productsRef = db.collection('product');
+  const query = productsRef.where('price', '>', 20);
+  
+  query.get()
+       .then(products => {
+         products.forEach(doc => {
+           data = doc.data()
+           document.write(`${data.name} at $${data.price} <br>`)
+         })
+       })
+  
+  
+  
+  
+  
+  
+  
+  
   //reference object in database table posts with id firstpost
-  const myPost = db.collection("post").doc("firstpost");
+ // const myPost = db.collection("post").doc("firstpost");
 
-  myPost.onSnapshot(doc => {
+  // myPost.onSnapshot(doc => {
 
-    const data = doc.data();
+  //   const data = doc.data();
 
-    document.querySelector('#title').innerHTML = data.title
+  //   document.querySelector('#title').innerHTML = data.title
 
 
-  })
+  // })
 
 
 //Request post from firestore
