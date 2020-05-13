6c0342feab3917b7efae391e61e9972553bf444d
Anisa <anisahamohamed@gmail.com>
2020-05-13 16:33:22 -0400

    data request from firestore

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ firebase login
 firebase init hosting
 firebase serve ---> serve app locally on port 5000
 firebase deploy --> deploy app to a live hosted url with an ssl certificate which gives support for https etc
+enable login type in firebase console then button in app.js will create authentication
--- public/app.js
+++ public/app.js
@@ -1,17 +1,32 @@
 document.addEventListener("DOMContentLoaded", (event) => {
+  
   const app = firebase.app();
-  console.log(app);
+  //reference database
+  const db = firebase.firestore();
+  //reference object in database table posts with id firstpost
+  const myPost = db.collection("post").doc("firstpost");
+
+  myPost.get()
+  .then(doc => {
+
+    const data = doc.data();
+    document.write(data.title + `<br>`)
+    console.log(data)
+    document.write(data.views )
+
+  })
+
 });
-//GOOGLE AUTHENTICATION
-function googleLogin() {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then((res) => {
-      const user = res.user;
-      document.write(`Hello ${user.displayName}`);
-      console.log(user)
-    })
-    .catch(console.log)
-};
+// //GOOGLE AUTHENTICATION
+// function googleLogin() {
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   firebase
+//     .auth()
+//     .signInWithPopup(provider)
+//     .then((res) => {
+//       const user = res.user;
+//       document.write(`Hello ${user.displayName}`);
+//       console.log(user);
+//     })
+//     .catch(console.log);
+// }
